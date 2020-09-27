8a98fa11627e75e293fa88c25d7d4a7b2cf3a0f1
Tereza Simkova <simkovat@gmail.com>
2020-09-27 22:39:44 +0200

    Add redux provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,19 @@ import * as serviceWorker from './serviceWorker';
 
 import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { store } from './store/store';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
