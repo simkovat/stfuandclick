18842ebdd535cebcc05142163011e510dca02308
Tereza Simkova <simkovat@gmail.com>
2020-09-28 15:49:49 +0200

    Tune 404 message

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -2,4 +2,6 @@ import { FC } from 'react';
 import { Layout } from '../components/Layout';
 import React from 'react';
 
-export const NotFoundPage: FC = () => <Layout>404</Layout>;
+export const NotFoundPage: FC = () => (
+  <Layout>404. We're sorry, this page was not found.</Layout>
+);
